bfe6dbaa41afde338eba1beb90d36238d8afab8d
LaZaRaS <yokesh.nsr2010@gmail.com>
2025-11-24 23:53:34 +0530

    Fully integrated Docker and also facilitated Multi-stage builds for prod and dev

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,9 +6,11 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
+    host: '0.0.0.0',
+    port: 5173,
     proxy: {
       '/api': {
-        target: "http://localhost:8080",
+        target: "http://server:8080",
         changeOrigin: true,
         secure: false,
         timeout: 30000
--- src/main/java/com/expensemonitor/CorsConfig.java
+++ src/main/java/com/expensemonitor/CorsConfig.java
@@ -1,3 +1,4 @@
+// This file is not needed for prod, since nginx manages the api calls
 package com.expensemonitor;
 
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -8,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
             registry.addMapping("/api/**")
-            .allowedOrigins("http://localhost:3000", "http://localhost:5173")
+            .allowedOrigins("http://localhost:3000", "http://localhost:5173", "http://frontend:80")
             .allowedMethods("GET", "POST", "PUT", "DELETE")
             .allowedHeaders("*")
             .allowCredentials(true)
